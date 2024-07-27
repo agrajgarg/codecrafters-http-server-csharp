cf70335e18551955fdd65eef4a85925e49155f4e
[Agraj Garg] <[agraj50@gmail.com]>
2024-07-28 03:24:51 +0530

    Debugging configuration changed

--- src/Server.cs
+++ src/Server.cs
@@ -9,20 +9,23 @@ await using NetworkStream stream = client.GetStream();
 // Read incoming data
 byte[] requestBuffer = new byte[1024];
 int bytesRead = await stream.ReadAsync(requestBuffer, 0, requestBuffer.Length);
-string request =
-    System.Text.Encoding.UTF8.GetString(requestBuffer, 0, bytesRead);
+string request = System.Text.Encoding.UTF8.GetString(requestBuffer, 0, bytesRead);
 Console.WriteLine($"Received request: {request}");
 var requestLines = request.Split("\r\n");
 var requests = requestLines[0].Split("/");
+for (int i = 0; i < requestLines.Length; i++)
+{
+    Console.WriteLine(requestLines[i]);
+}
 // Prepare and send response
 string response = "";
 if (requests[1] == " HTTP")
 {
     response = "HTTP/1.1 200 OK\r\n\r\n";
 }
-else if (requests[1] == "echo")
+else if (requests[1] == "user-agent HTTP")
 {
-    string str = requests[2].Split(" ")[0];
+    string str = requestLines[3].Split(": ")[1];
     int len = str.Length;
     response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + len + "\r\n\r\n" + str;
 }
